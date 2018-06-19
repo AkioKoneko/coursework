8df7f3b5b5fa7d4a31f312bb499beb8883f50e15
AkioKoneko <sashadetush@gmail.com>
2018-06-19 19:36:42 +0300

    Добавил поля и методы получения свойств в базовый класс

---
+++ phone.hpp
@@ -0,0 +1,35 @@
+#ifndef PHONE_HPP
+#define PHONE_HPP
+
+using mm = short;
+using MHz = short;
+using MB = short;
+
+class Phone
+{
+	short prod_year;
+	std::string color, comm_type;
+	bool has_gps, has_glonass, has_wifi, has_4g, has_sensor;
+	mm screen_diagonal;
+	MHz cpu_frequency;
+	float camera_mp;
+	MB memory_size;
+
+public:
+	auto   ProductionYear() const { return prod_year;            }
+	auto & CommType()       const { return comm_type;            }
+	auto & Color()          const { return color;                }
+	bool   HasGPS()         const { return has_gps;              }
+	bool   HasGLONASS()     const { return has_glonass;          }
+	bool   HasWiFi()        const { return has_wifi;             }
+	bool   Has4G()          const { return has_4g;               }
+	bool   HasSensor()      const { return has_sensor;           }
+	bool   HasCamera()      const { return camera_mp != 0;       }
+	bool   HasScreen()      const { return screen_diagonal != 0; }
+	auto   CameraMP()       const { return camera_mp;            }
+	mm     ScreenDiagonal() const { return screen_diagonal;      }
+	MHz    CPUFreqency()    const { return cpu_frequency;        }
+	MB     MemorySize()     const { return memory_size;          }
+};
+
+#endif
