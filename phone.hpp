a8366e5ed7c2781b42ccd0616c1eef1caf79ea52
AkioKoneko <sashadetush@gmail.com>
2018-06-20 21:07:39 +0300

     

--- phone.hpp
+++ phone.hpp
@@ -10,28 +10,51 @@ using MB = short;
 class Phone
 {
 	short prod_year;
-	std::string color, comm_type;
-	bool has_gps, has_glonass, has_wifi, has_4g, has_sensor;
+	std::string color;
+	std::string comm_type;
+
+public:
+	      auto ProductionYear() const { return prod_year; }
+	const auto & CommType()     const { return comm_type; }
+	const auto & Color()        const { return color;     }
+};
+
+class LandlinePhone : public Phone
+{
+
+};
+
+class MobilePhone : public Phone
+{
+	bool has_gps, has_glonass, has_wifi, has_4g;
 	mm screen_diagonal;
 	MHz cpu_frequency;
-	float camera_mp;
 	MB memory_size;
 
 public:
-	auto   ProductionYear() const { return prod_year;            }
-	auto & CommType()       const { return comm_type;            }
-	auto & Color()          const { return color;                }
-	bool   HasGPS()         const { return has_gps;              }
-	bool   HasGLONASS()     const { return has_glonass;          }
-	bool   HasWiFi()        const { return has_wifi;             }
-	bool   Has4G()          const { return has_4g;               }
-	bool   HasSensor()      const { return has_sensor;           }
-	bool   HasCamera()      const { return camera_mp != 0;       }
-	bool   HasScreen()      const { return screen_diagonal != 0; }
-	auto   CameraMP()       const { return camera_mp;            }
-	mm     ScreenDiagonal() const { return screen_diagonal;      }
-	MHz    CPUFreqency()    const { return cpu_frequency;        }
-	MB     MemorySize()     const { return memory_size;          }
+	bool HasGPS()         const { return has_gps;              }
+	bool HasGLONASS()     const { return has_glonass;          }
+	bool HasWiFi()        const { return has_wifi;             }
+	bool Has4G()          const { return has_4g;               }
+	bool HasCamera()      const { return camera_mp != 0;       }
+	bool HasScreen()      const { return screen_diagonal != 0; }
+	auto CameraMP()       const { return camera_mp;            }
+	mm   ScreenDiagonal() const { return screen_diagonal;      }
+	MHz  CPUFreqency()    const { return cpu_frequency;        }
+	MB   MemorySize()     const { return memory_size;          }
+};
+
+class Smartphone : public MobilePhone
+{
+	float camera_mp;
+
+public:
+	auto CameraMP() const { return camera_mp; }
+};
+
+class SatellitePhone : public MobilePhone
+{
+
 };
 
 #endif
